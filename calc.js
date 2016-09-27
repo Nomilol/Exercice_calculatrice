7ecbb6dfe872f47699f8eea813f35dc7b4d10180
Axel <lezinaxel@gmail.com>
2016-09-27 15:15:38 +0200

    Petit programme calculatrice qui marche

---
+++ calc.js
@@ -0,0 +1,52 @@
+function add(a,b){
+	var result = a + b;
+	return result;
+}
+
+function substract(a,b){
+	return a-b;
+
+}
+
+function multiply(a,b){
+	return a*b;
+
+}
+
+
+function calc(x,y){
+	var operation = process.argv[2];
+
+	var x = process.argv[3];
+	var y = process.argv[4];
+
+	var a = parseInt(x,10);
+	var b = parseInt(y,10);
+	
+	var result = add(a,b);
+	console.log(result);
+
+	if (operation === 'add'){
+		console.log(add(a,b));
+	}
+
+	else if(operation === 'multiply'){
+		console.log(multiply(a,b));
+	}
+
+	else{
+		console.log(substract(a,b));
+	}
+}
+
+calc();		
+
+
+
+
+
+
+
+
+
+
